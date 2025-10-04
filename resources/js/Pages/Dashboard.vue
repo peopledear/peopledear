9ed9678fef6274322aa652e21c6589bdecee709e
Francisco Barrento <francisco.barrento@gmail.com>
2025-10-04 17:18:25 +0100

    wip: dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const open = ref(true);
+</script>
 
 <template>
-    <div>Hello from dashboard</div>
+    <UApp>
+        <UDashboardGroup unit="rem">
+            <UDashboardSidebar
+                id="default"
+                v-model:open="open"
+                collapsible
+                resizable
+            >
+                <template #header>
+                    <h2 class="text-lg font-bold">PeopleDear</h2>
+                </template>
+            </UDashboardSidebar>
+            <slot />
+            <UDashboardPanel id="customers">
+                <template #header>Dashboard!</template>
+            </UDashboardPanel>
+        </UDashboardGroup>
+    </UApp>
 </template>
 
 <style scoped></style>

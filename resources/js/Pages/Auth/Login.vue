dbcc356f037a7a4dfd771b3ba44ead528bf2ce06
Francisco Barrento <francisco.barrento@gmail.com>
2025-10-04 12:24:31 +0100

    wip: adds inputs to login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import AuthLayout from "@/Layouts/AuthLayout.vue";
+import { ref } from "vue";
+
+const showPassword = ref(false);
+
 </script>
 
 <template>
@@ -9,6 +13,38 @@ import AuthLayout from "@/Layouts/AuthLayout.vue";
                 <div>Welcome to PeopleDear</div>
                 <div>Don't have an account? <a href="">Register</a></div>
             </template>
+            <div class="flex flex-col gap-y-6">
+                <UFormField label="Email" required>
+                    <UInput name="email" placeholder="Enter your email" width="full" class="w-full" />
+                </UFormField>
+                <UFormField label="Password" required>
+                    <UInput
+                        name="password"
+                        placeholder="Enter your password"
+                        width="full"
+                        class="w-full"
+                        :type="showPassword ? 'text' : 'password'"
+                        :ui="{trailing: 'pe-1'}"
+                    >
+                        <template #trailing>
+                            <UButton
+                                color="neutral"
+                                variant="link"
+                                size="sm"
+                                :icon="showPassword ? 'i-lucide-eye-off' : 'i-lucide-eye'"
+                                :aria-label="showPassword ? 'Hide password' : 'Show password'"
+                                :aria-pressed="showPassword"
+                                aria-controls="password"
+                                @click="showPassword = !showPassword"
+                            />
+                        </template>
+                    </UInput>
+                </UFormField>
+                <UFormField>
+                    <UCheckbox label="Remember me"/>
+                </UFormField>
+                <UButton label="Continue" block />
+            </div>
         </AuthLayout>
     </div>
 </template>
